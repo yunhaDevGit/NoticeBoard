aeeee60a96eab802ce02e7bd0db1e66904b1f5c9
yunha <jhy3990585@innogrid.com>
2021-04-30 12:13:14 +0900

    init commit

---
+++ src/test/java/com/toyproject/noticeboard/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
