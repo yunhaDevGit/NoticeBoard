3a374d792c25c2637640ec33fa87fa08ca2235ad
yunha <jhy3990585@innogrid.com>
2021-05-11 15:19:32 +0900

    feat: add template engine(Thymeleaf)

--- src/main/java/com/toyproject/noticeboard/controller/FreeBoardController.java
+++ src/main/java/com/toyproject/noticeboard/controller/FreeBoardController.java
@@ -3,27 +3,27 @@ package com.toyproject.noticeboard.controller;
 import com.toyproject.noticeboard.model.BoardInfo;
 import com.toyproject.noticeboard.repository.FreeBoardRepository;
 import com.toyproject.noticeboard.service.FreeBoardService;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/freeboard")
 public class FreeBoardController {
 
   @Autowired
   FreeBoardService freeBoardService;
   @Autowired
   FreeBoardRepository freeBoardRepository;
+
+  @GetMapping("/test")
+  public String test() {
+    return "test";
+  }
 
   @GetMapping("/list/all")
   @ResponseBody
